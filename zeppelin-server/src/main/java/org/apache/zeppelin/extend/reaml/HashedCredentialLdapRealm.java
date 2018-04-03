9472bd04d50200f7e3ffe3430a1adc1cb052734a
tigerchen <chenfj068@gmail.com>
2018-04-03 10:10:49 +0800

    HashedCredentialLdapRealm bug fix

--- zeppelin-server/src/main/java/org/apache/zeppelin/extend/reaml/HashedCredentialLdapRealm.java
+++ zeppelin-server/src/main/java/org/apache/zeppelin/extend/reaml/HashedCredentialLdapRealm.java
@@ -17,15 +17,15 @@
 
 package org.apache.zeppelin.extend.reaml;
 
-import javax.naming.NamingException;
-import javax.naming.ldap.LdapContext;
-
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.realm.ldap.LdapContextFactory;
 import org.apache.shiro.realm.ldap.LdapUtils;
-import org.apache.zeppelin.conf.ZeppelinConfiguration;
 import org.apache.zeppelin.realm.LdapRealm;
+
+import javax.naming.NamingException;
+import javax.naming.ldap.LdapContext;
 
 /**
  * HashedCredentialLdapRealm integrate to ldap custom hash.
@@ -34,20 +34,20 @@ public class HashedCredentialLdapRealm extends LdapRealm {
 
   @Override
   protected AuthenticationInfo queryForAuthenticationInfo(AuthenticationToken token,
-      LdapContextFactory ldapContextFactory)
+                                                          LdapContextFactory ldapContextFactory)
       throws NamingException {
 
     Object principal = token.getPrincipal();
     Object credentials = token.getCredentials();
     Md5PasswordEncoder encoder = new Md5PasswordEncoder();
     encoder.setEncodeHashAsBase64(true);
-    credentials = encoder.encodePassword(credentials.toString(), null);
+    credentials = encoder.encodePassword(new String((char[]) (credentials)), null);
     principal = getLdapPrincipal(token);
+    UsernamePasswordToken usernamePasswordToken = (UsernamePasswordToken) token;
+    usernamePasswordToken.setPassword(((String) credentials).toCharArray());
     LdapContext ctx = null;
     try {
       ctx = ldapContextFactory.getLdapContext(principal, credentials);
-      //context was opened successfully,
-      // which means their credentials were valid.  Return the AuthenticationInfo:
       return createAuthenticationInfo(token, principal, credentials, ctx);
     } finally {
       LdapUtils.closeContext(ctx);
